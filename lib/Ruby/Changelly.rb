f1b7c1957774e116ef26fe1ca7dc3dc7287b7dd9
Jason Crockett <jesusislord3@gmx.co.uk>
2019-01-26 13:04:36 +0000

    finished the api. version 1. untested.

--- lib/Ruby/Changelly.rb
+++ lib/Ruby/Changelly.rb
@@ -16,26 +16,28 @@ module Ruby
         @api_key = api_key
         @api_secret = api_secret
         @api_url = api_url
-
       end
       def login
-
+        raise "Not Implemented"
       end
       def logout
-
+        raise "Not Implemented"
       end
       def encrypted_message(api_secret=@api_secret,message)
         # https://stackoverflow.com/questions/24401623/equivalent-hmac-sha-512-key
         # code is licenced! https://meta.stackexchange.com/questions/271080/the-mit-license-clarity-on-using-code-on-stack-overflow-and-stack-exchange
         # https://legalict.com/2016/01/07/what-is-the-license-status-of-stackoverflow-code-snippets/
         # 2 programmers coming up with the same code argument applies here. facts are not covered by copyright.
-        @@encrypted = OpenSSL::Digest('sha512')
-        @@hmac = OpenSSL::HMAC.digest(@@encrypted,@api_secret,message)
+        # @@encrypted = OpenSSL::Digest('sha512')
+
+        # result was gained from here:
+        # https://ruby-doc.org/stdlib-2.4.2/libdoc/openssl/rdoc/OpenSSL/HMAC.html
+        @@hmac = OpenSSL::HMAC.digest("SHA512",api_secret,message.to_s)
         return @@hmac
       end
       def send_message(message = {'jsonrpc' => '2.0','id' => 'responselabel','method' => 'getExchangeAmount','params' => [{'from'=>'eur','to' => 'btc' ,'amount'=> '3'}]},api_secret=@api_secret)
         # https://old.changelly.com/developers#protocol
-        @@headers = {'api-key' => @api_key,'sign'=> encrypted_message(@@message)}
+        @@headers = {'api-key' => @api_key,'sign'=> encrypted_message(message)}
         # send the message
         @@tosend = @@headers.merge(message) # more ram rather than code golf but usable.
         # https://rubygems.org/gems/rpc4json
@@ -46,14 +48,14 @@ module Ruby
       end
       def getCurrenciesFull(id='ruby-changelly-getCurrenciesFull')
         # https://old.changelly.com/developers#currency-list
-        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getCurrenciesFull'}
+        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getCurrenciesFull','params' => {}}
          results = send_message @@message
          parsed_results = JSON.parse(results)
          return parsed_results["result"]
       end
       def getCurrencies(id='ruby-changelly-getCurrencies')
           # https://old.changelly.com/developers#currency-list
-         @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getCurrencies'}
+         @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getCurrencies','params' => {}}
          results = send_message @@message
          parsed_results = JSON.parse(results)
          return parsed_results["result"]
@@ -61,22 +63,47 @@ module Ruby
       def getMinAmount(from_currency,to_currency)
         # https://old.changelly.com/developers#currency-list
         # resume from above
-        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getMinAmount'}
+        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getMinAmount','params' =>
+            {'from' => from_currency,
+             'to' => to_currency}
+        }
         results = send_message @@message
         parsed_results = JSON.parse(results)
         return parsed_results["result"]
       end
-      def getExchangeAmount(from_currency,to_currency)
-      # gets the estimated amount of coins resulting from an exchange.
-      #   the estimated result includes the partner fee as well as the exchange fee
+      def getExchangeAmount(currencies_hash_array)
+        # gets the estimated amount of coins resulting from an exchange.
+        #   the estimated result includes the partner fee as well as the exchange fee
+        # https://old.changelly.com/developers#currency-list
+        # currencies_hash_array example is [{from => 'eur','to' => 'btc','amount' => 2.33 },{from => 'btc','eth' => 'btc','amount' => 30.0 },...]
+        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getExchangeAmount','params' => currencies_hash_array}
+        results = send_message @@message
+        parsed_results = JSON.parse(results)
+        return parsed_results["result"]
       end
-      def createTransaction(from_currency,to_currency,reciepient_address,extra_id,refund_address,refund_extra_id)
-
+      def createTransaction(from_currency,to_currency,recipient_address,amount,extra_id="null",refund_address="null",refund_extra_id="null")
+        # create a transaction to finalise by sending funds.
+        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'createTransaction','params' =>
+            {'from'=>from_currency,
+             'to_currency'=>to_currency,
+             'address' => recipient_address,
+             'extra_id' => extra_id ,
+             'amount'=>amount,
+             'refundAddress'=>refund_address,
+             'refundExtraId' => refund_extra_id }
+        }
+        results = send_message @@message
+        parsed_results = JSON.parse(results)
+        return parsed_results["result"]
       end
       def getStatus(transaction_id)
-
+        @@message = {'jsonrpc' => '2.0','id' => id.to_s,'method' => 'getStatus','params' =>
+            {'id' => transaction_id }
+        }
+        results = send_message @@message
+        parsed_results = JSON.parse(results)
+        return parsed_results["result"]
       end
-
     end
   end
 end
--- test/Ruby/Changelly_test.rb
+++ test/Ruby/Changelly_test.rb
@@ -1,6 +1,9 @@
 require "test_helper"
 require 'Ruby/Changelly/version'
 require 'Ruby/Changelly'
+require 'json'
+
+# A low priority work in progress. untested. test_api_key_is_valid is assumed to work
 
 class Ruby::ChangellyTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -8,10 +11,43 @@ class Ruby::ChangellyTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
+  end
+  def test_api_key_is_valid
+    createSettingsFile
+    begin
+      interface = Changelly::Changelly.new(readApiKey,readApiSecret)
+      if interface.send_message
+        result=true
+      end
+    rescue
+      result=false
+    end
+    assert result, "This Api Key is valid"
   end
   def test_assert_message_sent
-    interface = Changelly::Changelly.new()
-    interface.
+    createSettingsFile
+    interface = Changelly::Changelly.new(readApiKey,readApiSecret)
+    assert interface.getCurrencies, "get currencies works"
+  end
+  # begin helper functions
+  def createSettingsFile
+    if !File.exists? 'Settings.json'
+      puts "Creating Settings.json"
+      settings_file = File.new "Settings.json","W"
+      settings_hash = {'api_key' => "","api_secret" => "" }
+
+      settings_file.write(JSON.unparse settings_hash)
+
+      puts "Now fill in the file, located where modules are stored. its called Settings.json"
+    end
+  end
+  def readApiKey
+    settings_file = File.new "Settings.json","r"
+    return JSON.parse(settings_file)['api_key']
+  end
+  def readApiSecret
+    settings_file = File.new "Settings.json","r"
+    return JSON.parse(settings_file)['api_secret']
   end
 end
